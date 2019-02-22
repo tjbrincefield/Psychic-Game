77163e06f06a349f62f5decbce9d812aff06a92f
Tiffany B <tjbrincefield@gmail.com>
2019-02-21 19:24:37 -0500

    added more code to javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,56 @@
 
 
     var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var wins = 0;
+    var losses = 0;
+    var guessesLeft = 10;
+    var soFar = 0;
+    var guessesMade = [];
+    var directionsText = document.getElementById("directions-text");
+    var userChoiceText = document.getElementById("userchoice-text");
+    var computerChoiceText = document.getElementById("computerchoice-text");
+    var winsText = document.getElementById("wins-text");
+    var lossesText = document.getElementById("losses-text");
+    var guessesLeftText = document.getElementById("guessesleft-text");
+    var soFarText = document.getElementById("myguesses-text")
 
-        // Creating variables to hold the number of wins, losses, and ties. They start at 0.
-        var wins = 0;
-        var losses = 0;
-        var guessesLeft = 0;
-        var soFar = 0;
-
-        // Create variables that hold references to the places in the HTML where we want to display things.
-        var directionsText = document.getElementById("directions-text");
-        var userChoiceText = document.getElementById("userchoice-text");
-        var computerChoiceText = document.getElementById("computerchoice-text");
-        var winsText = document.getElementById("wins-text");
-        var lossesText = document.getElementById("losses-text");
-        var guessesLeft = document.getElementById("guessesleft-text");
-
-        // This function is run whenever the user presses a key.
         document.onkeyup = function(event) {
-
-        // Determines which key was pressed.
+            console.log(event)
         var userGuess = event.key;
 
-        // Randomly chooses a choice from the options array. This is the Computer's guess.
         var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-        // Reworked our code from last step to use "else if" instead of lots of if statements.
 
-        // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-        if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
-
-            if ((userGuess === "r" && computerGuess === "s") ||
-            (userGuess === "s" && computerGuess === "p") || 
-            (userGuess === "p" && computerGuess === "r")) {
-            wins++;
-            } else if (userGuess === computerGuess) {
-            ties++;
-            } else {
-            losses++;
+            if (guessesLeft === 0){
+                // guessesLeft  = (10());
+                //  How do i reset the guesses left back to 10 and increase the wins or losses once all the guesses have been used
             }
+            else if (userGuess == computerGuess){
+                wins++;
+                // guessesLeft = 10;
+                // how to reset once there has been a win in the column
+            }
+            else
+            {
+                guessesMade.push(" " + userGuess)
+                losses++;
+                guessesLeft--;
+            }
+            // else if (userGuess != computerGuess)
+            // {
+            //     losses++;
+            //     guessesLeft = 10;
+            // }
+            
 
-            // Hide the directions
+
             directionsText.textContent = "";
 
-            // Display the user and computer guesses, and wins/losses/ties.
-            userChoiceText.textContent = "You chose: " + userGuess;
-            computerChoiceText.textContent = "The computer chose: " + computerGuess;
-            winsText.textContent = "wins: " + wins;
-            lossesText.textContent = "losses: " + losses;
-            tiesText.textContent = "ties: " + ties;
-        }
+            document.getElementById("userchoice-text").textContent = "You chose: " + userGuess;
+            document.getElementById("computerchoice-text").textContent = "The computer chose: " + computerGuess;
+            document.getElementById("wins-text").textContent = "wins: " + wins;
+            document.getElementById("losses-text").textContent = "losses: " + losses;
+            document.getElementById("guessesleft-text").textContent = "Guesses remaining: " + guessesLeft;
+            document.getElementById("myguesses-text").textContent = "Guessed letters: " + guessesMade
+        
         };
